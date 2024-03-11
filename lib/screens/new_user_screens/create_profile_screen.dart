25a2bdf01dc2f2ff62490aaae1f3c442cf9c4aa8
Knightmare2X <vikashansdak@gmail.com>
2024-03-11 11:35:37 +0530

    uint8list photo added

--- lib/screens/new_user_screens/create_profile_screen.dart
+++ lib/screens/new_user_screens/create_profile_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:concord/model/persist_nav_bar.dart';
@@ -26,15 +25,18 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
-      setState(() {
-        selectedImagePath = pickedFile.path;
-      });
+      final Uint8List? bytes = await pickedFile.readAsBytes();
+     if (bytes != null) {
+       setState(() {
+         selectedImageBytes = bytes;
+       });
+     }
 
-      // Read the image file as bytes
-      List<int> imageBytes = await File(selectedImagePath!).readAsBytes();
-      // Convert the bytes to Uint8List
-      selectedImageBytes = Uint8List.fromList(imageBytes);
     }
+  }
+
+  Future<bool> _checkUniqueUsername(String username) async {
+    return await FirestoreMethods().isUsernameUnique(username);
   }
 
   @override
@@ -68,9 +70,9 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
                         child: selectedImageBytes != null
                             ? null
                             : Icon(Icons.person,
-                                size: 80,
-                                color: Colors
-                                    .white), // Show icon only if image is not loaded
+                            size: 80,
+                            color: Colors
+                                .white), // Show icon only if image is not loaded
                         backgroundImage: selectedImageBytes != null
                             ? MemoryImage(selectedImageBytes!)
                             : null,
@@ -102,7 +104,7 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
                       hintText: 'Username',
                       counterText: '',
                       errorText:
-                          showUsernameError ? 'Username cannot be empty' : null,
+                      showUsernameError ? 'Username cannot be empty' : null,
                     ),
                     textAlign: TextAlign.center,
                   ),
@@ -113,6 +115,25 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
                   padding: const EdgeInsets.all(16),
                   child: ElevatedButton(
                     onPressed: () async {
+                      if (username.isEmpty) {
+                        setState(() {
+                          showUsernameError = true;
+                        });
+                        return;
+                      }
+
+                      bool isUnique = await _checkUniqueUsername(username);
+
+                      if (!isUnique) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Username already exists."),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                        return;
+                      }
+
                       try {
                         final User user = FirebaseAuth.instance.currentUser!;
                         await FirestoreMethods().uploadData(
@@ -123,6 +144,10 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
                             duration: Duration(seconds: 2),
                           ),
                         );
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PersistNavBar()));
                       } catch (e) {
                         print("Error: $e");
                         ScaffoldMessenger.of(context).showSnackBar(
@@ -132,10 +157,6 @@ class _CreateProfileScreenState extends State<CreateProfileScreen> {
                           ),
                         );
                       }
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PersistNavBar()));
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.lightBlue.shade200,
